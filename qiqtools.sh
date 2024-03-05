0558da2d043c2711dfa3d81bd482e947c8135d47
ZhouQi-T5810 <gfjykldd@foxmail.com>
2024-03-05 11:46:42 +0800

    remove Emoji in main menu.

--- qiqtools.sh
+++ qiqtools.sh
@@ -3049,23 +3049,23 @@ ${green}QiQTools 一键脚本工具 $script_version
 ${plain}(支持Ubuntu|Debian|CentOS|Alpine系统)
 ${plain}-- 输入 >> ${yellow}qiq ${plain}<< 可快速启动此脚本 --
 ${plain}-------------------------------
-${green} 1${white}.${yellow}🟢系统信息${plain}
-${green} 2${white}.${plain}♾️系统更新
-${green} 3${blue}.${plain}🛑系统清理
+${green} 1${white}.${yellow}系统信息🟢${plain}
+${green} 2${white}.${plain}系统更新♾️
+${green} 3${blue}.${plain}系统清理🛑
 ${plain}----------------
-${green} 4${white}.${plain}🛠️常用工具 ▶
-${yellow} 5${white}.${yellow}⚙️系统工具 ${blue}▶
-${green} 6${white}.${plain}⚒️其他工具 ▶
-${green} 7${white}.${plain}🗺️面板工具 ▶
-${red} 8${white}.${red}✈️节点工具 ${white}▶ ${red}warp🚀 ${blue}x-ui
+${green} 4${white}.${plain}常用工具 ▶
+${yellow} 5${white}.${yellow}系统工具 ${blue}▶
+${green} 6${white}.${plain}其他工具 ▶
+${green} 7${white}.${plain}面板工具 ▶
+${red} 8${white}.${red}节点工具 ${white}▶ ${red}warp ${blue}x-ui
 ${plain}----------------
-${green} 9${white}.${plain}🌀容器管理 ▶
-${green}10${white}.${yellow}🛞站点管理 ▶${plain}
-${green}11${white}.${plain}🌐站点部署 ▶
+${green} 9${white}.${plain}容器管理 ▶
+${green}10${white}.${yellow}站点管理 ▶${plain}
+${green}11${white}.${plain}站点部署 ▶
 ${plain}-------------------------------
-${green}00.${plain}♻️脚本更新     ${green}99.${plain}🔁重启系统
+${green}00.${plain}脚本更新     ${green}99.${plain}重启系统
 ${plain}-------------------------------
-${green} 0.${plain}❌退出脚本
+${green} 0.${plain}退出脚本
 "
 }
 
