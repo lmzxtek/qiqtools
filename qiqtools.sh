7802700be43fd0f4d4d42dfbf8b49158753eaa27
ZhouQi-T5810 <gfjykldd@foxmail.com>
2024-03-05 11:58:06 +0800

    Update Emoji to main menu.

--- qiqtools.sh
+++ qiqtools.sh
@@ -3049,8 +3049,8 @@ ${green}QiQTools 一键脚本工具 $script_version
 ${plain}(支持Ubuntu|Debian|CentOS|Alpine系统)
 ${plain}-- 输入 >> ${yellow}qiq ${plain}<< 可快速启动此脚本 --
 ${plain}-------------------------------
-${green} 1${white}.${yellow}系统信息🟢${plain}
-${green} 2${white}.${plain}系统更新♾️
+${green} 1${white}.${yellow}系统信息👁️${plain}
+${green} 2${white}.${plain}系统更新💚
 ${green} 3${blue}.${plain}系统清理🛑
 ${plain}----------------
 ${green} 4${white}.${plain}常用工具 ▶
@@ -3063,9 +3063,10 @@ ${green} 9${white}.${plain}容器管理 ▶
 ${green}10${white}.${yellow}站点管理 ▶${plain}
 ${green}11${white}.${plain}站点部署 ▶
 ${plain}-------------------------------
-${green}00.${plain}脚本更新     ${green}99.${plain}重启系统
+${green}99.${plain}重启系统🌀
+${green}00.${plain}脚本更新✅     
 ${plain}-------------------------------
-${green} 0.${plain}退出脚本
+${green} 0.${plain}退出脚本❌
 "
 }
 
