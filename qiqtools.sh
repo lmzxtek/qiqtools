86d6e1d0e84b56f8b397f2edf5418f94290d7a29
ZhouQi-T5810 <gfjykldd@foxmail.com>
2024-03-06 16:48:17 +0800

    Update IP show info, and add system virtualize type.

--- qiqtools.sh
+++ qiqtools.sh
@@ -139,14 +139,14 @@ check_system_ip() {
   WAN4=$(expr "$IP4" : '.*query\":[ ]*\"\([^"]*\).*') &&
   COUNTRY4=$(expr "$IP4" : '.*country\":[ ]*\"\([^"]*\).*') &&
   ASNORG4=$(expr "$IP4" : '.*isp\":[ ]*\"\([^"]*\).*') &&
-  ipv4_address=$IP4 &&
+  ipv4_address=$WAN4 &&
   [[ "$L" = C && -n "$COUNTRY4" ]] && COUNTRY4=$(translate "$COUNTRY4")
 
   IP6=$(wget -6 -qO- --no-check-certificate --user-agent=Mozilla --tries=2 --timeout=1 https://api.ip.sb/geoip) &&
   WAN6=$(expr "$IP6" : '.*ip\":[ ]*\"\([^"]*\).*') &&
   COUNTRY6=$(expr "$IP6" : '.*country\":[ ]*\"\([^"]*\).*') &&
   ASNORG6=$(expr "$IP6" : '.*isp\":[ ]*\"\([^"]*\).*') &&
-  ipv6_address=$IP6 &&
+  ipv6_address=$WAN6 &&
   [[ "$L" = C && -n "$COUNTRY6" ]] && COUNTRY6=$(translate "$COUNTRY6")
 }
 
