ebc2caa4144e124e412e616d44847865fa6e2a45
ZhouQi-T5810 <gfjykldd@foxmail.com>
2024-03-06 11:06:36 +0800

    Add a char to version.

--- qiqtools.sh
+++ qiqtools.sh
@@ -3214,7 +3214,7 @@ ${cyan}23${white}.${cyan}节点工具${yellow}✈ ${red}warp${plain}
 ${white}-------------------------------------${plain}
 ${green}99.${plain}重启系统${blue}❂${plain}       ${green}00.${plain}脚本更新${blue}♨${plain}
 ${white}=====================================${plain}
-${green} 0.${plain}退出脚本${red}✖${plain}       ${green}♧${blue}QiQTools-$script_version${plain}
+${green} 0.${plain}退出脚本${red}✖${plain}       ${green}♧♧ ${blue}QiQTools-$script_version${plain}
 "
 }
 
