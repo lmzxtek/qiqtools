9a79e8216ab4bd2a184805c4d57062a646bed701
T5810 <lmzxtek@gmail.com>
2025-03-03 20:12:32 +0800

    Check qiq is set or not.

--- update_log.sh
+++ update_log.sh
@@ -167,5 +167,5 @@ echoY "   1.脚本链接url使用变量"
 echoY "   2.脚本更新添加链接地址"
 echoY "   3.检测qiq命令是否设置"
 echoT "--------------------------------"
-echoR "" $(echoY "脚本链接") $(echoR ": $url_script") 
-echoR "" $(echoY "重定向  ") $(echoR ": $url_redir") 
+echoR "" $(echoY "脚本 ") $(echoR ": $url_script") 
+echoR "" $(echoY "重定向") $(echoR ": $url_redir") 
